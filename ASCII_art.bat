79071f2ec33341134e2de3352e84f65a668dd0eb
bryanmax9 <brillant16.gcv@gmail.com>
2023-03-04 10:23:11 -0800

    ASCII Arts

---
+++ ASCII_art.bat
@@ -0,0 +1,57 @@
+@echo off
+
+chcp 65001
+echo.
+echo.
+echo.
+echo Bear
+echo ʕ•ᴥ•ʔ
+echo (v v)
+echo.
+echo ʕ-ᴥ-ʔ
+echo (v v)
+echo.
+echo Robot
+echo  ___ 
+echo ^|O_O^|
+echo /^| ^|\ 
+echo  u u 
+echo.
+echo  ___ 
+echo ^|-_-^|
+echo /^| ^|\ 
+echo  u u 
+echo.
+echo Cat
+echo  ^___^ 
+echo (^>‘-‘^<)
+echo  (u u)/ 
+echo.
+echo  ^___^ zZzZ 
+echo (^>^-^<)
+echo  (u u)/ 
+echo.
+echo Fizz
+echo   ____ 
+echo ψ(._. )^>
+echo.
+echo   ____ 
+echo ψ(-_- )^>
+echo.
+echo Kirby
+echo   ______ 
+echo ༼ つ ◕-◕ ༽つ 
+echo.
+echo   _______ zZzZz 
+echo ༼ つ ^> - ^< ༽つ 
+echo.
+echo Fat cat
+echo  /ᐠ｡ꞈ｡ᐟ\ 
+echo ( U  U ) 
+echo ^^^^^^^^ 
+echo.
+echo  /ᐠvꞈvᐟ\ zZzZ 
+echo ( U  U ) 
+echo ^^^^^^^^ 
+echo.
+PAUSE
